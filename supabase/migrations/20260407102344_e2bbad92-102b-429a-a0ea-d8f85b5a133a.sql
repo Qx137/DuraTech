295f93325040afacdade20ccb1454fc29513153b
Q137 <adorevengesai7@gmail.com>
2026-08-12 15:56:39 +0200

    fixes

--- supabase/migrations/20260407102344_e2bbad92-102b-429a-a0ea-d8f85b5a133a.sql
+++ supabase/migrations/20260407102344_e2bbad92-102b-429a-a0ea-d8f85b5a133a.sql
@@ -1,4 +1,4 @@
 ALTER TABLE public.deliveries
-ADD COLUMN transport_type text,
-ADD COLUMN offered_price numeric,
-ADD COLUMN min_price numeric;
+ADD COLUMN IF NOT EXISTS transport_type text,
+ADD COLUMN IF NOT EXISTS offered_price numeric,
+ADD COLUMN IF NOT EXISTS min_price numeric;
